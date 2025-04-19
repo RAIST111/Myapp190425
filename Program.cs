2bcf81412b8291a330707d0517edba08dcc17194
RAIST111 <Urijzrilin02487@gmail.com>
2025-04-19 12:59:50 +0300

    AddedTitle

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 {
     private static void Main(string[] args)
     {
+        System.Console.WriteLine("програма вычитания");
        int a = int.Parse(Console.ReadLine());
        int b = int.Parse(Console.ReadLine());
        System.Console.WriteLine(a - b);
