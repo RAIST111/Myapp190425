3b4704a4fdc7c31818c623c43eee015c48393ef7
RAIST111 <Urijzrilin02487@gmail.com>
2025-04-19 12:00:10 +0300

    Init

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+       int a = int.Parse(Console.ReadLine());
+       int b = int.Parse(Console.ReadLine());
+       System.Console.WriteLine(a + b);
+    }
+}
